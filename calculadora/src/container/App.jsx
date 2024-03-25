5752492b8d591b517dd2be1392ac1a3b44da1a3d
VeroVelas <veronicavelasco2778@gmail.com>
2024-03-25 14:51:02 -0600

    calculadora y logica

---
+++ calculadora/src/container/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Calculadora from '../pages/Calculadora'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Calculadora/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
